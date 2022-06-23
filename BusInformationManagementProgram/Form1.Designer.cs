9b19ac9eb91c7f19c6b2e588ffb8e695ac35043e
Guitarband <60247328+Guitarband@users.noreply.github.com>
2022-06-23 12:24:52 +1000

    Updated C# code and removed two unnecessary forms

--- BusInformationManagementProgram/Form1.Designer.cs
+++ BusInformationManagementProgram/Form1.Designer.cs
@@ -29,81 +29,28 @@ namespace BusInformationManagementProgram
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblCreateEditRecordLabel = new System.Windows.Forms.Label();
-            this.lblBIMPTitle = new System.Windows.Forms.Label();
             this.lblBIMPFull = new System.Windows.Forms.Label();
-            this.btnCreateRecord = new System.Windows.Forms.Button();
-            this.btnEditRecord = new System.Windows.Forms.Button();
-            this.lblTodayData = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblBIMPTitle = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.busNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.arrivalStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ArrivalStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.arrivalTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.averageArrivalTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.departureTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.arrivalDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lblCreateEditRecordLabel
-            // 
-            this.lblCreateEditRecordLabel.AutoSize = true;
-            this.lblCreateEditRecordLabel.Font = new System.Drawing.Font("VALORANT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCreateEditRecordLabel.Location = new System.Drawing.Point(456, 103);
-            this.lblCreateEditRecordLabel.Name = "lblCreateEditRecordLabel";
-            this.lblCreateEditRecordLabel.Size = new System.Drawing.Size(276, 24);
-            this.lblCreateEditRecordLabel.TabIndex = 0;
-            this.lblCreateEditRecordLabel.Text = "Create/Edit Record";
-            // 
-            // lblBIMPTitle
-            // 
-            this.lblBIMPTitle.AutoSize = true;
-            this.lblBIMPTitle.Font = new System.Drawing.Font("VALORANT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBIMPTitle.Location = new System.Drawing.Point(539, 9);
-            this.lblBIMPTitle.Name = "lblBIMPTitle";
-            this.lblBIMPTitle.Size = new System.Drawing.Size(110, 32);
-            this.lblBIMPTitle.TabIndex = 1;
-            this.lblBIMPTitle.Text = "BIMP";
             // 
             // lblBIMPFull
             // 
             this.lblBIMPFull.AutoSize = true;
-            this.lblBIMPFull.Font = new System.Drawing.Font("VALORANT", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBIMPFull.Location = new System.Drawing.Point(364, 41);
+            this.lblBIMPFull.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBIMPFull.Location = new System.Drawing.Point(433, 48);
             this.lblBIMPFull.Name = "lblBIMPFull";
-            this.lblBIMPFull.Size = new System.Drawing.Size(484, 20);
+            this.lblBIMPFull.Size = new System.Drawing.Size(317, 23);
             this.lblBIMPFull.TabIndex = 2;
             this.lblBIMPFull.Text = "Bus Information Management Program";
-            // 
-            // btnCreateRecord
-            // 
-            this.btnCreateRecord.Font = new System.Drawing.Font("VALORANT", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCreateRecord.Location = new System.Drawing.Point(479, 145);
-            this.btnCreateRecord.Name = "btnCreateRecord";
-            this.btnCreateRecord.Size = new System.Drawing.Size(108, 23);
-            this.btnCreateRecord.TabIndex = 3;
-            this.btnCreateRecord.Text = "Create Record";
-            this.btnCreateRecord.UseVisualStyleBackColor = true;
-            this.btnCreateRecord.Click += new System.EventHandler(this.btnCreateRecord_Click);
-            // 
-            // btnEditRecord
-            // 
-            this.btnEditRecord.Font = new System.Drawing.Font("VALORANT", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditRecord.Location = new System.Drawing.Point(602, 145);
-            this.btnEditRecord.Name = "btnEditRecord";
-            this.btnEditRecord.Size = new System.Drawing.Size(108, 23);
-            this.btnEditRecord.TabIndex = 4;
-            this.btnEditRecord.Text = "Edit Record";
-            this.btnEditRecord.UseVisualStyleBackColor = true;
-            this.btnEditRecord.Click += new System.EventHandler(this.btnEditRecord_Click);
-            // 
-            // lblTodayData
-            // 
-            this.lblTodayData.AutoSize = true;
-            this.lblTodayData.Font = new System.Drawing.Font("VALORANT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTodayData.Location = new System.Drawing.Point(495, 224);
-            this.lblTodayData.Name = "lblTodayData";
-            this.lblTodayData.Size = new System.Drawing.Size(188, 24);
-            this.lblTodayData.TabIndex = 5;
-            this.lblTodayData.Text = "Today\'s Data";
             // 
             // dataGridView1
             // 
@@ -111,47 +58,84 @@ namespace BusInformationManagementProgram
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.busNumber,
-            this.arrivalStatus,
+            this.ArrivalStatus,
             this.arrivalTime,
-            this.averageArrivalTime});
+            this.departureTime,
+            this.arrivalDate});
             this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveBorder;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 272);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 146);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(1160, 377);
             this.dataGridView1.TabIndex = 6;
             // 
+            // lblBIMPTitle
+            // 
+            this.lblBIMPTitle.AutoSize = true;
+            this.lblBIMPTitle.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBIMPTitle.Location = new System.Drawing.Point(539, 9);
+            this.lblBIMPTitle.Name = "lblBIMPTitle";
+            this.lblBIMPTitle.Size = new System.Drawing.Size(89, 39);
+            this.lblBIMPTitle.TabIndex = 1;
+            this.lblBIMPTitle.Text = "BIMP";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(142, 107);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "View Data For:";
+            // 
             // busNumber
             // 
+            this.busNumber.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.busNumber.HeaderText = "Bus Number";
             this.busNumber.Name = "busNumber";
             // 
-            // arrivalStatus
+            // ArrivalStatus
             // 
-            this.arrivalStatus.HeaderText = "Arrival Status";
-            this.arrivalStatus.Name = "arrivalStatus";
+            this.ArrivalStatus.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ArrivalStatus.HeaderText = "At School?";
+            this.ArrivalStatus.Name = "ArrivalStatus";
             // 
             // arrivalTime
             // 
+            this.arrivalTime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.arrivalTime.HeaderText = "Arrival Time";
             this.arrivalTime.Name = "arrivalTime";
             // 
-            // averageArrivalTime
+            // departureTime
             // 
-            this.averageArrivalTime.HeaderText = "Average Arrival Time";
-            this.averageArrivalTime.Name = "averageArrivalTime";
+            this.departureTime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.departureTime.HeaderText = "Departure Time";
+            this.departureTime.Name = "departureTime";
+            // 
+            // arrivalDate
+            // 
+            this.arrivalDate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.arrivalDate.HeaderText = "Date";
+            this.arrivalDate.Name = "arrivalDate";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.lblTodayData);
-            this.Controls.Add(this.btnEditRecord);
-            this.Controls.Add(this.btnCreateRecord);
             this.Controls.Add(this.lblBIMPFull);
             this.Controls.Add(this.lblBIMPTitle);
-            this.Controls.Add(this.lblCreateEditRecordLabel);
             this.Name = "MainForm";
             this.Text = "BIMP";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -162,18 +146,16 @@ namespace BusInformationManagementProgram
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblCreateEditRecordLabel;
-        private System.Windows.Forms.Label lblBIMPTitle;
         private System.Windows.Forms.Label lblBIMPFull;
-        private System.Windows.Forms.Button btnCreateRecord;
-        private System.Windows.Forms.Button btnEditRecord;
-        private System.Windows.Forms.Label lblTodayData;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblBIMPTitle;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridViewTextBoxColumn busNumber;
-        private System.Windows.Forms.DataGridViewTextBoxColumn arrivalStatus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ArrivalStatus;
         private System.Windows.Forms.DataGridViewTextBoxColumn arrivalTime;
-        private System.Windows.Forms.DataGridViewTextBoxColumn averageArrivalTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn departureTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn arrivalDate;
     }
 }
 
--- BusInformationManagementProgram/Form1.cs
+++ BusInformationManagementProgram/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace BusInformationManagementProgram
 {
@@ -14,11 +15,11 @@ namespace BusInformationManagementProgram
     {
         class BusRecord
         {
-            int busNumber;
-            int arrivalTime;
-            bool arrivalStatus;
-            int averageArrivalTime;
-            int arrivalDate;
+            public int busNumber;
+            public int arrivalDate;
+            public int arrivalTime;
+            public int departTime;
+            public bool atSchool;
         }
 
         void GetTime()
@@ -36,25 +37,87 @@ namespace BusInformationManagementProgram
 
         }
 
+        public string dateTimeFormat(int dateTime, string type)
+        {
+            if(type == "date")
+            {
+                string date = Convert.ToString(dateTime);
+                date.Insert(date.Length - 2, ":");
+                return date;
+
+            }
+            else
+            {
+                string time = Convert.ToString(dateTime);
+                time.Insert(time.Length - 2, ":");
+                return time;
+            }
+        }
+
+        public void ReadFile(int busNumber, string type)
+        {
+            if(type == "school")
+            {
+                type = "SchoolBuses/";
+            }
+            else
+            {
+                type = "SportBuses/";
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(type +"BusData_"+busNumber+".xml");
+
+            XmlNodeList xmlBusRecord = doc.GetElementsByTagName("BusData");
+
+            busRecord = new BusRecord[xmlBusRecord.Count];
+
+            int index = 0;
+            foreach (XmlNode busData in xmlBusRecord)
+            {
+                BusRecord b = new BusRecord();
+                int arrivalDate = Convert.ToInt32(busData.Attributes["date"].Value);
+                int arrivalTime = Convert.ToInt32(busData["arrivalTime"].InnerText);
+                int departTime = Convert.ToInt32(busData["departureTime"].InnerText);
+                bool status = Convert.ToBoolean(busData["atSchool"].InnerText);
+
+                b.busNumber = busNumber;
+                b.arrivalDate = arrivalDate;
+                b.arrivalTime = arrivalTime;
+                b.departTime = departTime;
+                b.atSchool = status;
+
+                busRecord[index] = b;
+                index++;
+            }
+        }
+
+        public void DisplayData()
+        {
+            BusRecord b = busRecord[0];
+            string status;
+            if(b.atSchool == true)
+            {
+                status = "Yes";
+            }
+            else
+            {
+                status = "No";
+            }
+            string[] data = { b.busNumber.ToString(), status, dateTimeFormat(b.arrivalTime, "time"), dateTimeFormat(b.departTime, "time"), dateTimeFormat(b.arrivalDate, "date") };
+            dataGridView1.Rows.Add(data);
+        }
+
 
         public MainForm()
         {
             InitializeComponent();
         }
 
+        BusRecord[] busRecord;
         private void MainForm_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void btnCreateRecord_Click(object sender, EventArgs e)
-        {
-            new CreateRecordForm().Show();
-        }
-
-        private void btnEditRecord_Click(object sender, EventArgs e)
-        {
-            new EditRecordForm().Show();
+            ReadFile(18, "school");
+            DisplayData();
         }
     }
 }
